deaf85fd2ccd31eb0f932b103d0c868512ff0921
Mauricio <mauriurdaneta20@gmail.com>
2025-04-09 21:58:17 -0500

    agregando funcion de registro de rutas

--- config.py
+++ config.py
@@ -4,8 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://postgres:mauricioj18@localhost/zapatos'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:mauricioj18@localhost/zapatos'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =True
-    app.config['SECRET_KEY'] ='mi_clave_secreta'
+    
     db.init_app(app)
+
+    from routes import register_routes
+    register_routes(app)
     return app
