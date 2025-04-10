fd003af37164695f1ad1d4be9d351b7b43eb39c9
Mauricio <mauriurdaneta20@gmail.com>
2025-04-09 21:56:59 -0500

    documento que corre el servidor e inicializador de modelo de base de datos

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
-from config import create_app
-
+from config import create_app,db
 app = create_app()
 
 
 if __name__=='__main__':
+    with app.app_context():
+        db.create_all()
+        print("Base de datos inicializada con  éxito 🎉🎉")    
     app.run(debug=True,host='0.0.0.0',port=5000)
 
